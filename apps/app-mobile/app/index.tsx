7cbd492f41903cb816a1562cf535fe0ad8013e1a
Adam Teale <adam@reign.cl>
2025-04-17 20:44:23 -0400

    react 18 working in mobile and web apps

---
+++ apps/app-mobile/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { DemoScreen } from "@Presentation/DemoScreen/DemoScreen";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <DemoScreen message="Hello from the mobile!" />
+    </View>
+  );
+}
