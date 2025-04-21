9a175805ae9ec21ae4645531c0c0ed4ea7640150
Adam Teale <adam@reign.cl>
2025-04-20 21:10:05 -0400

    debugging vercel errors

--- apps/app-core/src/presentation/screens/CartScreen/styledComponents/styledComponents.web.tsx
+++ apps/app-core/src/presentation/screens/CartScreen/styledComponents/styledComponents.web.tsx
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 import { StyleAStyles, StyleBStyles, StyleCStyles } from "./styles";
+import { ThemeType } from "@mono-repo-demo/atomic-library";
 
-const StyledContainerExample = styled.div`
+const StyledContainerExample = styled.div<{ theme: ThemeType }>`
   ${StyleAStyles}
 `;
 
-const StyledB = styled.div`
+const StyledB = styled.div<{ theme: ThemeType }>`
   ${StyleBStyles}
 `;
 
-const StyledC = styled.div`
+const StyledC = styled.div<{ theme: ThemeType }>`
   ${StyleCStyles}
 `;
 
