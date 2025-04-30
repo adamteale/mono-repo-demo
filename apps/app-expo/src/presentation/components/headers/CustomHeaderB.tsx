8ec9d86181e9236f7f0df5387b2e6c9ffb277b21
Adam Teale <adam@reign.cl>
2025-04-30 06:15:21 -0400

    updated next

--- apps/app-expo/src/presentation/components/headers/CustomHeaderB.tsx
+++ apps/app-expo/src/presentation/components/headers/CustomHeaderB.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-
-import { useNavigation } from "expo-router";
 import { StatusBar, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useNavigation } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
-import getStyles from "./styles";
-
 export function CustomHeaderB() {
-  const styles = getStyles();
+  const { top } = useSafeAreaInsets();
   const navigation = useNavigation();
-  return (
-    <View style={styles.customHeader}>
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
 
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={styles.backButton}
-      >
+  const headerDynamicStyle = {
+    height: top + 60,
+    paddingTop: top + 10,
+  };
+
+  return (
+    <View
+      className="bg-backgroundPrimary py-4 px-5 flex-row items-center justify-between gap-2"
+      style={headerDynamicStyle}
+    >
+      <StatusBar barStyle="light-content" backgroundColor="#182958" />
+
+      <TouchableOpacity onPress={() => navigation.goBack()} className="p-[5px]">
         <Ionicons name="arrow-back" size={40} color="white" />
       </TouchableOpacity>
     </View>
